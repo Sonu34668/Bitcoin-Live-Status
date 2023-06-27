c49716eeb176dda0aaf2a5deeb5c879640c1cf42
Sonu Kumar <sonu.kumar.descent@gmail.com>
2023-06-27 14:47:59 +0530

    corrected

--- script.js
+++ script.js
@@ -1,70 +1,77 @@
-function sortTable() {
-    var table, rows, switching, i, x, y, shouldSwitch;
-    table = document.getElementById("myTable");
-    switching = true;
-    /*Make a loop that will continue until
-    no switching has been done:*/
-    while (switching) {
-      //start by saying: no switching is done:
-      switching = false;
-      rows = table.rows;
-      /*Loop through all table rows (except the
-      first, which contains table headers):*/
-      for (i = 1; i < (rows.length - 1); i++) {
-        //start by saying there should be no switching:
-        shouldSwitch = false;
-        /*Get the two elements you want to compare,
-        one from current row and one from the next:*/
-        x = rows[i].getElementsByTagName("P")[0];
-        y = rows[i + 1].getElementsByTagName("P")[0];
-        //check if the two rows should switch place:
-        if (Number(x.innerHTML) > Number(y.innerHTML)) {
-          //if so, mark as a switch and break the loop:
-          shouldSwitch = true;
-          break;
-        }
-      }
-      if (shouldSwitch) {
-        /*If a switch has been marked, make the switch
-        and mark that a switch has been done:*/
-        rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
-        switching = true;
-      }
+let data = [];
+
+fetch(
+  "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false"
+)
+  .then((response) => response.json())
+  .then((dataResponse) => {
+    data = dataResponse;
+    console.log(data);
+    // Call Display function
+    renderTable(data);
+  })
+  .catch((error) => console.error("Error:", error));
+
+//display function
+
+function renderTable(data) {
+  console.log(data, "display function called");
+  const tableBody = document.getElementById("tableBody");
+  tableBody.innerHTML = "";
+  data.forEach((item) => {
+    const row = document.createElement("tr");
+    const percentageChange = item.price_change_percentage_24h;
+    const percentageChangeClass =
+      percentageChange >= 0 ? "positive-change" : "negative-change";
+
+    row.innerHTML = `
+        <td id="data1"><img src="${item.image}" alt="${
+      item.name
+    }" width="20"/></td>
+        <td>${item.name}</td>
+      <td>${item.symbol}</td>
+      <td>${item.id}</td>
+      <td>${"$" + item.current_price}</td>
+      <td class="${percentageChangeClass}">${
+      item.price_change_percentage_24h
+    }</td>
+    <td>${"Mkt Cap : $"+item.total_volume}</td>
+        `;
+    tableBody.appendChild(row);
+  });
+}
+
+//search function
+document.getElementById('searchInput').addEventListener('keyup',event=>{
+    const searchTerm=document.getElementById('searchInput').value.trim().toLowerCase();
+    if(searchTerm==''){
+        renderTable(data);
+        return;
     }
-  }
-  function sortTable1() {
-    var table, rows, switching, i, x, y, shouldSwitch;
-    table = document.getElementById("myTable");
-    switching = true;
-    /*Make a loop that will continue until
-    no switching has been done:*/
-    while (switching) {
-      //start by saying: no switching is done:
-      switching = false;
-      rows = table.rows;
-      /*Loop through all table rows (except the
-      first, which contains table headers):*/
-      for (i = 1; i < (rows.length - 1); i++) {
-        //start by saying there should be no switching:
-        shouldSwitch = false;
-        /*Get the two elements you want to compare,
-        one from current row and one from the next:*/
-        x = rows[i].getElementsByTagName("A")[0];
-        y = rows[i + 1].getElementsByTagName("A")[0];
-        //check if the two rows should switch place:
-        if (Number(x.innerHTML) > Number(y.innerHTML)) {
-          //if so, mark as a switch and break the loop:
-          shouldSwitch = true;
-          break;
-        }
-      }
-      if (shouldSwitch) {
-        /*If a switch has been marked, make the switch
-        and mark that a switch has been done:*/
-        rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
-        switching = true;
-      }
-    }
-  }
-let d =fetch("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false")
+
+    const filteredData=data.filter(item=>{
+        const itemName=item.name.toLowerCase();
+        const itemSymbol=item.symbol.toLowerCase();
+        return itemName.includes(searchTerm)|| itemSymbol.includes(searchTerm);
+    })
+// Bitcoin=>bitcoin user=>bit
+    renderTable(filteredData);
+})
+
+
+//sort function 1
+document.getElementById("sortMarketCapButton").addEventListener("click",()=>{
+    // console.log("market functional called")
+    data.sort((a,b)=>b.total_volume-a.total_volume);
+    renderTable(data);
+})
+
+
+
+//sort function 2
+document.getElementById("sortPercentageChangeButton").addEventListener("click",()=>{
+    // console.log("market functional called")
+    data.sort((a,b)=>a.price_change_percentage_24h-b.price_change_percentage_24h);
+    renderTable(data);
+})
 
